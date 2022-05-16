21f1934f884b794adc4b5fe7ae134714803b9890
paveldolgov4 <dolgoff1272@gmail.com>
2022-05-16 20:23:13 +0300

    translator

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -9,6 +9,12 @@ import android.util.Log
 import android.view.View
 import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.tasks.Task
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.common.model.RemoteModelManager
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.TranslateRemoteModel
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
@@ -19,17 +25,44 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+    private val options = TranslatorOptions.Builder()
+        .setSourceLanguage(TranslateLanguage.ENGLISH)
+        .setTargetLanguage(TranslateLanguage.RUSSIAN)
+        .build()
+
+    val englishRussianTranslator = Translation.getClient(options)
+    val conditions = DownloadConditions.Builder()
+        .requireWifi()
+        .build()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val translator = Translation.getClient(options)
+        lifecycle.addObserver(translator)
         val button = findViewById<View>(R.id.button)
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.img);
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.img_3);
         val image = InputImage.fromBitmap(bitmap, 0)
+
+
         recognizer.process(image)
             .addOnSuccessListener { text ->
-                // Task completed successfully
-                textView2.text = text.text
+                englishRussianTranslator.downloadModelIfNeeded(conditions)
+                    .addOnSuccessListener {
+                        englishRussianTranslator.translate(text.text)
+                            .addOnSuccessListener { translatedText ->
+                                textView2.text = translatedText
+                            }
+                            .addOnFailureListener { exception ->
+                                // Error.
+                                // ...
+                            }
+                    }
+                    .addOnFailureListener { exception ->
+                        // Model couldn’t be downloaded or other internal error.
+                        // ...
+                    }
             }
             .addOnFailureListener { exception ->
                 // Task failed with an exception
